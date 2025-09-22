2d6576f38112993af6e8a10ef9a699150b6a9232
Lior CHAMLA <lior.chamla@instant-system.com>
2025-09-22 11:47:00 +0200

    tests: repairs flacky tests that needed db to work

--- app/src/test/java/com/devmeetups/app/openapi/OpenApiContractTests.java
+++ app/src/test/java/com/devmeetups/app/openapi/OpenApiContractTests.java
@@ -9,15 +9,32 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+import com.devmeetups.app.events.repository.EventsRepository;
+import com.devmeetups.app.registrations.repository.RegistrationsRepository;
+
+@SpringBootTest(properties = {
+    "spring.flyway.enabled=false",
+    "spring.autoconfigure.exclude=" +
+        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
+@AutoConfigureMockMvc(addFilters = false)
 class OpenApiContractTests {
 
   @Autowired
   MockMvc mvc;
+
+  // 🔧 Mocke ce que tes controllers injectent (service ou repo)
+  @MockBean
+  EventsRepository eventsRepository;
+  @MockBean
+  RegistrationsRepository registrationsRepository;
 
   @Test
   void api_docs_exposes_events_paths() throws Exception {
